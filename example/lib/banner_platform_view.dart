8917e60f97b17ebaff004d8e1ec791efa26ca8c0
badar <bahadur@gaya.app>
2024-08-17 15:10:13 +0500

    flippable banner view

---
+++ example/lib/banner_platform_view.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
+
+class DigitalTurbineBannerView extends StatefulWidget {
+  final String placementId;
+  final double width;
+  final double height;
+
+  const DigitalTurbineBannerView({
+    Key? key,
+    required this.placementId,
+    this.width = 300,
+    this.height = 250,
+  }) : super(key: key);
+
+  @override
+  _DigitalTurbineBannerViewState createState() => _DigitalTurbineBannerViewState();
+}
+
+class _DigitalTurbineBannerViewState extends State<DigitalTurbineBannerView> {
+  late MethodChannel _channel;
+
+  @override
+  void initState() {
+    super.initState();
+    _channel = MethodChannel('digital_turbine_banner_view_${widget.placementId}');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This is used for iOS
+    if (defaultTargetPlatform == TargetPlatform.iOS) {
+      return Container(
+        color: Colors.red,
+        width: widget.width,
+        height: widget.height,
+        child: UiKitView(
+          viewType: 'digital_turbine_banner_view',
+          creationParams: {'placementId': widget.placementId},
+          creationParamsCodec: const StandardMessageCodec(),
+          onPlatformViewCreated: _onPlatformViewCreated,
+          gestureRecognizers: const <Factory<OneSequenceGestureRecognizer>>{},
+          hitTestBehavior: PlatformViewHitTestBehavior.opaque,
+        ),
+      );
+    }
+    // For Android, you would use AndroidView here
+    else if (defaultTargetPlatform == TargetPlatform.android) {
+      return Container(
+          color: Colors.red,
+          width: widget.width,
+          height: widget.height,
+        child: AndroidView(
+          viewType: 'digital_turbine_banner_view',
+          creationParams: {'placementId': widget.placementId},
+          creationParamsCodec: const StandardMessageCodec(),
+          onPlatformViewCreated: _onPlatformViewCreated,
+        ),
+      );
+    }
+    // For other platforms, show a placeholder
+    else {
+      return SizedBox(
+        width: widget.width,
+        height: widget.height,
+        child: Text('Banner ads not supported on this platform'),
+      );
+    }
+  }
+
+  void _onPlatformViewCreated(int id) {
+    _channel = MethodChannel('digital_turbine_banner_view_$id');
+    _loadAd();
+  }
+
+  Future<void> _loadAd() async {
+    try {
+      await _channel.invokeMethod('loadAd');
+    } on PlatformException catch (e) {
+      print("Failed to load ad: ${e.message}");
+    }
+  }
+
+  @override
+  void dispose() {
+    _disposeAd();
+    super.dispose();
+  }
+
+  Future<void> _disposeAd() async {
+    try {
+      await _channel.invokeMethod('disposeAd');
+    } on PlatformException catch (e) {
+      print("Failed to dispose ad: ${e.message}");
+    }
+  }
+}
